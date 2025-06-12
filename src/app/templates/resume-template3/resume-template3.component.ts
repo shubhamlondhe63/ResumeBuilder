5d643e87d175b45aa2f6309e990d456e8cad573e
Shubham Londhe <shubhamlondhe63@gmail.com>
2025-06-12 18:46:50 +0530

    Template 3 added

--- src/app/templates/resume-template3/resume-template3.component.ts
+++ src/app/templates/resume-template3/resume-template3.component.ts
@@ -8,4 +8,14 @@ import { Component, Input } from '@angular/core';
 })
 export class ResumeTemplate3Component {
   @Input() formData: any;
+
+  getInitials(): string {
+    if (!this.formData?.fullName) {
+      return 'NA';
+    }
+    return this.formData.fullName
+      .split(' ')
+      .map((name: string) => name.charAt(0).toUpperCase())
+      .join('');
+  }
 }
